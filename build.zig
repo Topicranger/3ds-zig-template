5913af08b330cb0e5009d575aca6fc55586c64ee
Topicranger <krusti@fluffytech.xyz>
2025-07-31 17:21:58 +0200

    Follow style guide

--- build.zig
+++ build.zig
@@ -7,19 +7,19 @@ const Env = struct {
 const env: Env = @import("env.zig.zon");
 
 // 3DS values
-const appFilename = "apptemplate";
-const appTitle = "App template";
-const appDescription = "Built with Zig, devkitARM, and libctru";
-const appAuthor = "YourName";
+const app_filename = "apptemplate";
+const app_title = "App template";
+const app_description = "Built with Zig, devkitARM, and libctru";
+const app_author = "YourName";
 
 // devkitPro paths
-const devkitPro = "/opt/devkitpro"; //or the path to your installation, e.g. "c:/devkitPro"
-const devkitProToolsDir = devkitPro ++ "/tools/bin";
-const devkitARMCompilerDir = devkitPro ++ "/devkitARM/bin";
-const devkitARMIncludeDir = devkitPro ++ "/devkitARM/arm-none-eabi/include";
-const ctruIncludeDir = devkitPro ++ "/libctru/include";
-const ctruLibDir = devkitPro ++ "/libctru/lib";
-const ctruDir = devkitPro ++ "/libctru";
+const devkitpro = "/opt/devkitpro"; //or the path to your installation, e.g. "c:/devkitPro"
+const devkitpro_tools_dir = devkitpro ++ "/tools/bin";
+const devkitarm_compiler_dir = devkitpro ++ "/devkitARM/bin";
+const devkitarm_include_dir = devkitpro ++ "/devkitARM/arm-none-eabi/include";
+const ctru_include_dir = devkitpro ++ "/libctru/include";
+const ctru_lib_dir = devkitpro ++ "/libctru/lib";
+const ctru_dir = devkitpro ++ "/libctru";
 
 pub fn build(b: *std.Build) void {
     // step 1
@@ -28,10 +28,10 @@ pub fn build(b: *std.Build) void {
 
     // check if all required tools are available before providing an option to
     // build for the 3DS
-    const dkp3dsx = b.findProgram(&.{"3dsxtool" ++ extension}, &.{devkitProToolsDir}) catch "";
-    const dkpsmdh = b.findProgram(&.{"smdhtool" ++ extension}, &.{devkitProToolsDir}) catch "";
-    const dkpgcc = b.findProgram(&.{"arm-none-eabi-gcc" ++ extension}, &.{devkitARMCompilerDir}) catch "";
-    if (dkp3dsx.len == 0 or dkpsmdh.len == 0 or dkpgcc.len == 0) {
+    const dkp_3dsx = b.findProgram(&.{"3dsxtool" ++ extension}, &.{devkitpro_tools_dir}) catch "";
+    const dkp_smdh = b.findProgram(&.{"smdhtool" ++ extension}, &.{devkitpro_tools_dir}) catch "";
+    const dkp_gcc = b.findProgram(&.{"arm-none-eabi-gcc" ++ extension}, &.{devkitarm_compiler_dir}) catch "";
+    if (dkp_3dsx.len == 0 or dkp_smdh.len == 0 or dkp_gcc.len == 0) {
         return;
     }
 
@@ -45,20 +45,20 @@ pub fn build(b: *std.Build) void {
     };
 
     const obj = b.addObject(.{
-        .name = appFilename,
+        .name = app_filename,
         .target = b.resolveTargetQuery(target),
         .optimize = optimize,
         .root_source_file = b.path("src/main.zig"), //TODO: refactor to modules
     });
     obj.linkLibC();
-    obj.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = devkitPro ++ "/portlibs/3ds/include" } });
-    obj.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = ctruIncludeDir } });
-    obj.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = devkitARMIncludeDir } });
+    obj.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = devkitpro ++ "/portlibs/3ds/include" } });
+    obj.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = ctru_include_dir } });
+    obj.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = devkitarm_include_dir } });
 
     const wf = b.addWriteFiles();
 
     // step 3
-    const elf = b.addSystemCommand(&(.{dkpgcc}));
+    const elf = b.addSystemCommand(&(.{dkp_gcc}));
 
     elf.setCwd(wf.getDirectory());
     elf.addArgs(&.{
@@ -69,35 +69,35 @@ pub fn build(b: *std.Build) void {
         "-mfloat-abi=hard",
         "-mtp=soft",
     });
-    _ = elf.addPrefixedOutputFileArg("-Wl,-Map,", appFilename ++ ".map");
+    _ = elf.addPrefixedOutputFileArg("-Wl,-Map,", app_filename ++ ".map");
     elf.addArtifactArg(obj);
     elf.addArgs(&.{
-        "-L" ++ ctruLibDir,
+        "-L" ++ ctru_lib_dir,
         "-lctru",
     });
-    const out_elf = elf.addPrefixedOutputFileArg("-o", appFilename ++ ".elf");
+    const out_elf = elf.addPrefixedOutputFileArg("-o", app_filename ++ ".elf");
 
     // step 4
-    const icon = ctruDir ++ "/default_icon.png";
-    const smdh = b.addSystemCommand(&.{dkpsmdh});
+    const icon = ctru_dir ++ "/default_icon.png";
+    const smdh = b.addSystemCommand(&.{dkp_smdh});
     smdh.setCwd(wf.getDirectory());
     smdh.addArgs(&.{
         "--create",
-        appTitle,
-        appDescription,
-        appAuthor,
+        app_title,
+        app_description,
+        app_author,
         icon,
     });
-    const out_smdh = smdh.addOutputFileArg(appFilename ++ ".smdh");
+    const out_smdh = smdh.addOutputFileArg(app_filename ++ ".smdh");
 
     // step 5
-    const dsx = b.addSystemCommand(&.{dkp3dsx});
+    const dsx = b.addSystemCommand(&.{dkp_3dsx});
     dsx.setCwd(wf.getDirectory());
     dsx.addFileArg(out_elf);
-    const out_dsx = dsx.addOutputFileArg(appFilename ++ ".3dsx");
+    const out_dsx = dsx.addOutputFileArg(app_filename ++ ".3dsx");
     dsx.addPrefixedFileArg("--smdh=", out_smdh);
 
-    const install_3dsx = b.addInstallFileWithDir(out_dsx, .prefix, appFilename ++ ".3dsx");
+    const install_3dsx = b.addInstallFileWithDir(out_dsx, .prefix, app_filename ++ ".3dsx");
     const ds_step = b.step("3ds", "Build 3DS executable");
     ds_step.dependOn(&install_3dsx.step);
 
@@ -117,7 +117,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&emulate.step);
 
     // Run on device
-    const dslink = b.findProgram(&.{"3dslink"}, &.{devkitProToolsDir}) catch "3dslink";
+    const dslink = b.findProgram(&.{"3dslink"}, &.{devkitpro_tools_dir}) catch "3dslink";
     const upload = b.addSystemCommand(&.{ dslink, "-a", env.ip_3ds });
     upload.addFileArg(out_dsx);
     const upload_step = b.step("launch", "Run in The Homebrew Launcher via 3dslink NetLoader");
